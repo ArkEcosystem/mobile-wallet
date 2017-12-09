58794775b8926ffad711331329e900d543e415c2
Lucio Rubens <luciorubeens@gmail.com>
2017-12-09 12:05:28 -0300

    improvement: hide the toolbar when scrolling

--- src/directives/directives.module.ts
+++ src/directives/directives.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { KeyboardAttachDirective } from './keyboard-attach/keyboard-attach';
 import { MainnetOnlyDirective } from './mainnet-only/mainnet-only';
+import { HeaderScrollerDirective } from './header-scroller/header-scroller';
 
 @NgModule({
-	declarations: [KeyboardAttachDirective, MainnetOnlyDirective],
+	declarations: [KeyboardAttachDirective, MainnetOnlyDirective, HeaderScrollerDirective],
 	imports: [],
-	exports: [KeyboardAttachDirective, MainnetOnlyDirective]
+	exports: [KeyboardAttachDirective, MainnetOnlyDirective, HeaderScrollerDirective]
 })
 export class DirectivesModule {}
---
+++ src/directives/header-scroller/header-scroller.ts
@@ -0,0 +1,64 @@
+import { Directive, ElementRef, OnInit } from '@angular/core';
+
+/**
+ * Directive based in https://github.com/ionic-team/ionic/issues/1381
+ * with some changes
+ */
+
+@Directive({
+	selector: '[header-scroller]'
+})
+export class HeaderScrollerDirective implements OnInit {
+  private triggerDistance: number = 100;
+	private transition: number = 0.5;
+  private el: HTMLElement;
+  private lastScrollTop: number = 0;
+
+	constructor(el: ElementRef) {
+		this.el = el.nativeElement;
+	}
+
+	public ngOnInit() {
+		let el = this.el;
+		let children = el && el.children;
+		let childList = children && <Array<HTMLElement>>Array.from(children);
+
+		let scrollList = (childList || []).filter(e => e.classList.contains('scroll-content'));
+    let scroll = scrollList.length ? scrollList[0] : null;
+
+    // Set animation transition
+    scroll.style.transition = `margin ${this.transition}s linear`;
+    scroll.style.zIndex = '998';
+    scroll.style.backgroundColor = 'inherit';
+
+		if (scroll) {
+			this.bindScroller(scroll);
+		}
+	}
+
+	private bindScroller(el: HTMLElement): void {
+		el.addEventListener('scroll', event => {
+      let scroller = <HTMLElement>event.target;
+
+      let header = <HTMLElement>scroller.parentElement.previousElementSibling;
+      let nav = <HTMLElement>scroller.parentElement.previousElementSibling.children[0];
+
+      let scrollTop = scroller.scrollTop;
+      let scrollerStyle = scroller.style;
+
+      let navOffset = nav.offsetHeight;
+      let headerOffset = header.offsetHeight;
+
+      let newMargin;
+      if (scrollTop > this.lastScrollTop && scrollTop >= this.triggerDistance) {
+        newMargin = navOffset;
+      } else if (scrollTop < this.lastScrollTop && scrollTop <= this.triggerDistance) {
+        newMargin = headerOffset;
+        scroller.scrollTop = 0;
+      }
+
+      scrollerStyle.marginTop = `${newMargin}px`;
+      this.lastScrollTop = scrollTop;
+		});
+	}
+}
