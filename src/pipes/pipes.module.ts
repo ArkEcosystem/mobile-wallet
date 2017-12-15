31f23bdc4de16b135f7e1cadb45d6145e4306bf2
Lucio Rubens <luciorubeens@gmail.com>
2017-12-15 08:20:38 -0300

    improvement: display date in user's timezone

--- src/pipes/pipes.module.ts
+++ src/pipes/pipes.module.ts
@@ -2,14 +2,20 @@ import { NgModule } from '@angular/core';
 import { TruncateMiddlePipe } from './../pipes/truncate-middle/truncate-middle';
 import { UnitsSatoshiPipe } from './../pipes/units-satoshi/units-satoshi';
 import { AccountLabelPipe } from './../pipes/account-label/account-label';
+import { TimestampHumanPipe } from './timestamp-human/timestamp-human';
+import { TimezonePipe } from './timezone/timezone';
 
 @NgModule({
 	declarations: [TruncateMiddlePipe,
     UnitsSatoshiPipe,
-    AccountLabelPipe],
+    AccountLabelPipe,
+    TimestampHumanPipe,
+    TimezonePipe],
 	imports: [],
 	exports: [TruncateMiddlePipe,
     UnitsSatoshiPipe,
-    AccountLabelPipe]
+    AccountLabelPipe,
+    TimestampHumanPipe,
+    TimezonePipe]
 })
 export class PipesModule {}
