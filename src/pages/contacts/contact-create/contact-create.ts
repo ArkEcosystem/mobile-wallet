1d7a51014b8136927645b0e795058ac00f95b2d2
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2017-12-28 13:17:14 +0000

    Fix editing Contact name
    Resolves #21

--- src/pages/contacts/contact-create/contact-create.ts
+++ src/pages/contacts/contact-create/contact-create.ts
@@ -25,6 +25,7 @@ export class ContactCreatePage {
 
   public contact: Contact;
   public address: string;
+  public contactName: string;
 
   private currentNetwork;
 
@@ -39,6 +40,7 @@ export class ContactCreatePage {
 
     this.isNew = lodash.isEmpty(param);
     this.contact = this.isNew ? new Contact() : param;
+    this.contactName = this.contact.name;
     this.currentNetwork = this.userDataProvider.currentNetwork;
   }
 
@@ -55,6 +57,7 @@ export class ContactCreatePage {
       return;
     }
 
+    this.contact.name = this.contactName;
     if (this.isNew) {
       this.userDataProvider.addContact(this.address, this.contact);
     } else {
