4540c469819f4d164a81835890432e76c3e30aff
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2018-01-15 11:11:28 +0000

    Standardize QR Code scanner

--- src/pages/transaction/transaction-send/transaction-send.ts
+++ src/pages/transaction/transaction-send/transaction-send.ts
@@ -143,7 +143,7 @@ export class TransactionSendPage implements OnInit{
   }
 
   scanQRCode() {
-    this.qrScanner.open();
+    this.qrScanner.open(true);
   }
 
   onInputToken() {
@@ -172,11 +172,11 @@ export class TransactionSendPage implements OnInit{
     });
   }
 
-  onScanQRCode(qrCode: object) {
-    if (qrCode['a']) {
+  onScanQRCode(qrCode: QRCodeScheme) {
+    if (qrCode.address) {
       this.contact = null;
-      this.transaction.recipientAddress = qrCode['a'];
-      this.searchBar.inputElem.value = qrCode['a'];
+      this.transaction.recipientAddress = qrCode.address;
+      this.searchBar.inputElem.value = qrCode.address;
     } else {
       this.toastProvider.error('QR_CODE.INVALID_QR_ERROR');
     }
