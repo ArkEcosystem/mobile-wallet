6645977af8c8807125657f5a809930e554a033b5
Patrick Zenhäusern <pz@tenhouse.ch>
2018-03-14 21:53:17 +0100

    add optional amount to receive

--- src/pages/transaction/transaction-receive/transaction-receive.module.ts
+++ src/pages/transaction/transaction-receive/transaction-receive.module.ts
@@ -4,6 +4,7 @@ import { TransactionReceivePage } from './transaction-receive';
 
 import { TranslateModule } from '@ngx-translate/core';
 import { QRCodeComponentModule } from '@components/qr-code/qr-code.module';
+import { AmountComponentModule } from '@components/amount/amount.module';
 
 @NgModule({
   declarations: [
@@ -12,6 +13,7 @@ import { QRCodeComponentModule } from '@components/qr-code/qr-code.module';
   imports: [
     IonicPageModule.forChild(TransactionReceivePage),
     TranslateModule,
+    AmountComponentModule,
     QRCodeComponentModule,
   ],
 })
