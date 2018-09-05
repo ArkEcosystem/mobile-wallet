b7fa3b4c555efc145fc32d3f3598966c40b41237
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2018-09-05 11:22:50 +0100

    fix: custom networks reset with mainnet on signin

--- src/modals/custom-network-create/custom-network-create.ts
+++ src/modals/custom-network-create/custom-network-create.ts
@@ -47,6 +47,7 @@ export class CustomNetworkCreateModal {
         this.network.activePeer = new Peer();
         this.network.activePeer.ip = seedServerUrl.hostname;
         this.network.activePeer.port = Number(seedServerUrl.port);
+        this.network.type = 10
         this.dismiss(this.network);
       }, () => this.configureError());
   }
