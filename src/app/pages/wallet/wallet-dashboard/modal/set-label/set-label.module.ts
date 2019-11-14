b96008c4b3469458073134b10d663ce17ad8b440
Lucio Rubens <4539235+luciorubeens@users.noreply.github.com>
2019-11-14 09:47:26 -0300

    refactor: set label page

--- src/app/pages/wallet/wallet-dashboard/modal/set-label/set-label.module.ts
+++ src/app/pages/wallet/wallet-dashboard/modal/set-label/set-label.module.ts
@@ -5,6 +5,7 @@ import { SetLabelPage } from './set-label';
 import { TranslateModule } from '@ngx-translate/core';
 import { DirectivesModule } from '@/directives/directives.module';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -12,6 +13,7 @@ import { CommonModule } from '@angular/common';
   ],
   imports: [
     IonicModule,
+    FormsModule,
     CommonModule,
     DirectivesModule,
     TranslateModule,
