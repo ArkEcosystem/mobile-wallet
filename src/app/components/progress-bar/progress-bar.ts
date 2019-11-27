edda84129ad23bb4ad67a454b7de3ad2128e693f
Lucio Rubens <4539235+luciorubeens@users.noreply.github.com>
2019-11-27 09:50:32 -0300

    refactor: update generate entropy page

--- src/app/components/progress-bar/progress-bar.ts
+++ src/app/components/progress-bar/progress-bar.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'progress-bar',
   templateUrl: 'progress-bar.html',
+  styleUrls: ['progress-bar.pcss'],
   inputs: ['progress'],
 })
 export class ProgressBarComponent {
