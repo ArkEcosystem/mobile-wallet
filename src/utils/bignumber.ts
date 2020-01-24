bfa2dbb429b3b2d9877d79c5eae3b4a8c2243628
Lucio Rubens <4539235+luciorubeens@users.noreply.github.com>
2020-01-24 09:13:20 -0300

    chore: delete all the things

--- src/utils/bignumber.ts
+++
@@ -1,5 +0,0 @@
-import BigNumber from 'bignumber.js';
-
-// Avoid scientific notation
-// https://github.com/MikeMcl/bignumber.js/blob/master/bignumber.d.ts#L97
-export default BigNumber.clone({ DECIMAL_PLACES: 8, EXPONENTIAL_AT: 1e+9 });
