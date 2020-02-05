5c734fbea5b0c9e194dfaf040395708ffd859fbc
Caio Alcantara <alcantaracaiolucas@gmail.com>
2020-02-05 17:27:01 -0300

    refac: Bring back the test file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -105,6 +105,7 @@ export class AppComponent implements OnDestroy, OnInit {
 			this.initBackButton();
 			this.splashScreen.hide();
 
+			// TODO: Transfer this responsibility to intro module init
 			// this.authProvider.hasSeenIntro().subscribe(hasSeenIntro => {
 			// 	if (!hasSeenIntro) {
 			// 		this.openPage("/intro", true);
