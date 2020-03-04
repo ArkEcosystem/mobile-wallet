5988a10a7ca1738ce62f9fe5f0668ad67ab65989
Lucio Rubens <4539235+luciorubeens@users.noreply.github.com>
2020-03-04 08:50:31 -0300

    fix: ensure only valid addresses in the current network

--- src/app/components/wallet-picker/wallet-picker.modal.ts
+++ src/app/components/wallet-picker/wallet-picker.modal.ts
@@ -1,4 +1,5 @@
 import { Contact, Wallet } from "@/models/model";
+import { ArkApiProvider } from "@/services/ark-api/ark-api";
 import { UserDataProvider } from "@/services/user-data/user-data";
 import { Component, OnInit } from "@angular/core";
 import { ModalController } from "@ionic/angular";
@@ -14,6 +15,7 @@ export class WalletPickerModal implements OnInit {
 	constructor(
 		private modalCtrl: ModalController,
 		private userDataProvider: UserDataProvider,
+		private arkApiProvider: ArkApiProvider,
 	) {}
 
 	ngOnInit() {
@@ -21,10 +23,15 @@ export class WalletPickerModal implements OnInit {
 		const network = this.userDataProvider.currentNetwork;
 		this.symbol = network.symbol;
 		this.contacts = Object.values(profile.contacts);
-		this.wallets = Object.values(profile.wallets).map((wallet: Wallet) => ({
-			name: wallet.label,
-			address: wallet.address,
-		}));
+		this.wallets = Object.values(profile.wallets)
+			// ensure only valid addresses in the current network
+			.filter((wallet: Wallet) =>
+				this.arkApiProvider.validateAddress(wallet.address),
+			)
+			.map((wallet: Wallet) => ({
+				name: wallet.label,
+				address: wallet.address,
+			}));
 	}
 
 	onPick(wallet: Contact) {
