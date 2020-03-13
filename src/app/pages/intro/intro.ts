2bde7e6157cfacc9b960373dd4d88e28db0e60ee
Lúcio Rubens <4539235+luciorubeens@users.noreply.github.com>
2020-03-13 14:12:45 -0300

    test: refactor intro page (#524)
    * chore: exclude files from coverage
    
    * test: refactor intro page

--- src/app/pages/intro/intro.ts
+++ src/app/pages/intro/intro.ts
@@ -1,5 +1,11 @@
-import { Component, ViewChild } from "@angular/core";
-import { IonSlides, NavController, Platform } from "@ionic/angular";
+import {
+	AfterViewInit,
+	Component,
+	OnInit,
+	ViewChild,
+	ViewEncapsulation,
+} from "@angular/core";
+import { IonSlides, NavController } from "@ionic/angular";
 import { TranslateService } from "@ngx-translate/core";
 
 import { AuthProvider } from "@/services/auth/auth";
@@ -9,50 +15,49 @@ import { AuthProvider } from "@/services/auth/auth";
 	templateUrl: "intro.html",
 	styleUrls: ["intro.pcss"],
 })
-export class IntroPage {
-	@ViewChild("slider", { read: IonSlides, static: true })
-	slider: IonSlides;
-
+export class IntroPage implements OnInit {
 	public showSkip = true;
-	public slides: any;
+	public slides: any[];
 	public activeIndex = 0;
 
+	@ViewChild(IonSlides)
+	public slider: IonSlides;
+
 	constructor(
-		platform: Platform,
 		private navCtrl: NavController,
 		private authProvider: AuthProvider,
 		private translateService: TranslateService,
-	) {
-		platform.ready().then(() => {
-			this.translateService
-				.get([
-					"INTRO_PAGE.WELCOME",
-					"INTRO_PAGE.TEXT_1",
-					"INTRO_PAGE.SECURITY",
-					"INTRO_PAGE.TEXT_2",
-					"INTRO_PAGE.FAST_EASY",
-					"INTRO_PAGE.TEXT_3",
-				])
-				.subscribe(translation => {
-					this.slides = [
-						{
-							title: translation["INTRO_PAGE.WELCOME"],
-							image: "anytime",
-							description: translation["INTRO_PAGE.TEXT_1"],
-						},
-						{
-							title: translation["INTRO_PAGE.SECURITY"],
-							image: "pincode",
-							description: translation["INTRO_PAGE.TEXT_2"],
-						},
-						{
-							title: translation["INTRO_PAGE.FAST_EASY"],
-							image: "fast-easy",
-							description: translation["INTRO_PAGE.TEXT_3"],
-						},
-					];
-				});
-		});
+	) {}
+
+	ngOnInit() {
+		this.translateService
+			.get([
+				"INTRO_PAGE.WELCOME",
+				"INTRO_PAGE.TEXT_1",
+				"INTRO_PAGE.SECURITY",
+				"INTRO_PAGE.TEXT_2",
+				"INTRO_PAGE.FAST_EASY",
+				"INTRO_PAGE.TEXT_3",
+			])
+			.subscribe(translation => {
+				this.slides = [
+					{
+						title: translation["INTRO_PAGE.WELCOME"],
+						image: "anytime",
+						description: translation["INTRO_PAGE.TEXT_1"],
+					},
+					{
+						title: translation["INTRO_PAGE.SECURITY"],
+						image: "pincode",
+						description: translation["INTRO_PAGE.TEXT_2"],
+					},
+					{
+						title: translation["INTRO_PAGE.FAST_EASY"],
+						image: "fast-easy",
+						description: translation["INTRO_PAGE.TEXT_3"],
+					},
+				];
+			});
 	}
 
 	startApp() {
@@ -65,7 +70,7 @@ export class IntroPage {
 	}
 
 	goNext() {
-		this.slider.slideNext();
+		return this.slider.slideNext();
 	}
 
 	async onSlideChanged() {
@@ -75,7 +80,6 @@ export class IntroPage {
 		if (activeIndex >= slideLength) {
 			return;
 		}
-
 		this.activeIndex = activeIndex;
 		this.showSkip = !(await this.slider.isEnd());
 	}
