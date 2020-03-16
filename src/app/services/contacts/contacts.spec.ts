d85e19b7701b21b43245c6c546a7d9a73e638989
Caio Alcantara <alcantaracaiolucas@gmail.com>
2020-03-16 16:39:25 -0300

    test: add empty name case

--- src/app/services/contacts/contacts.spec.ts
+++ src/app/services/contacts/contacts.spec.ts
@@ -1,5 +1,6 @@
 import { TestBed } from "@angular/core/testing";
 
+import { StoredNetwork } from "@/models/model";
 import { NetworkProvider } from "@/services/network/network";
 import { UserDataService } from "@/services/user-data/user-data.interface";
 
@@ -10,14 +11,18 @@ const INVALID_ADDRESS = "D8x2Rno1CxrE5kRoVHS1EooQnfTL3v5z12";
 
 fdescribe("Contacts service", () => {
 	let contactsService: ContactsProvider;
+	let userDataService: UserDataService;
 
 	beforeEach(function() {
 		TestBed.configureTestingModule({
 			// imports: [IonicStorageModule.forRoot()],
 			providers: [ContactsProvider, NetworkProvider, UserDataService],
 		});
-
 		contactsService = TestBed.inject(ContactsProvider);
+		userDataService = TestBed.inject(UserDataService);
+		const network = new StoredNetwork();
+		network.version = 30;
+		userDataService.currentNetwork = network;
 	});
 
 	it("should throw invalid address error", done => {
@@ -25,6 +30,16 @@ fdescribe("Contacts service", () => {
 			() => {},
 			error => {
 				expect(error.key).toEqual("CONTACTS_PAGE.INVALID_ADDRESS");
+				done();
+			},
+		);
+	});
+
+	it("should throw invalid name empty error", done => {
+		contactsService.addContact(VALID_ADDRESS, null).subscribe(
+			() => {},
+			error => {
+				expect(error.key).toEqual("CONTACTS_PAGE.CONTACT_NAME_EMPTY");
 				done();
 			},
 		);
