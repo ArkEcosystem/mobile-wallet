6c17000cee36c45f6801229b034b4a97fc4f8c71
Lucio Rubens <4539235+luciorubeens@users.noreply.github.com>
2020-04-07 10:06:07 -0300

    fix: update icons

--- src/app/auth/auth-touch-id/auth-touch-id.component.ts
+++ src/app/auth/auth-touch-id/auth-touch-id.component.ts
@@ -5,6 +5,7 @@ import { Store } from "@ngxs/store";
 @Component({
 	selector: "auth-touch-id",
 	templateUrl: "auth-touch-id.component.html",
+	styleUrls: ["auth-touch-id.component.pcss"],
 })
 export class AuthTouchIdComponent {
 	constructor(private store: Store, private fingerprint: FingerprintAIO) {}
