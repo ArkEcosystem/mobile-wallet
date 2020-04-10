a8f78a7d6fe77238c4cad699f11dff1fed4a680b
Caio Alcântara <alcantaracaiolucas@gmail.com>
2020-04-10 12:11:02 -0300

    test: add ark api test cases (#560)
    * test: [wip] add initial structure
    
    * fix: activate network
    
    * test: wip covering public get methods
    
    * fix: should complete
    
    * test: wip - get network fee statistics
    
    * test: wip network fee statistics
    
    * test: wip cover more cases in statistics context
    
    * test: wip add fees test case and fixture
    
    * fix: node/fees request
    
    * test: wip add cached fees case
    
    * test: wip should use cached fees case
    
    * test: wip should fetch and return delegates
    
    * fix: cached fees
    
    * fix: skip delegates
    
    * refactor: fetch fees
    
    * test: wip inscrease ark api coverage
    
    * test: wip: add validateAddress and getDelegateByPk
    
    * test: adjust network version
    
    * ci: fix webdriver version
    
    Co-authored-by: Lucio Rubens <4539235+luciorubeens@users.noreply.github.com>

---
+++ test/fixture/delegates.fixture.ts
@@ -0,0 +1,602 @@
+export default [
+	{
+		username: "biz_classic",
+		address: "AKdr5d9AMEnsKYxpDcoHdyyjSCKVx3r9Nj",
+		publicKey:
+			"020431436cf94f3c6a6ba566fe9e42678db8486590c732ca6c3803a10a86f50b92",
+		votes: "311980193794464",
+		rank: 1,
+		isResigned: false,
+		blocks: {
+			produced: 211835,
+			last: {
+				id:
+					"07dae9bf06a6037c475d108eb204b1a43feeaa351c8b1acf050e42ebd128ebe1",
+				height: 11807739,
+				timestamp: {
+					epoch: 95638680,
+					unix: 1585739880,
+					human: "2020-04-01T11:18:00.000Z",
+				},
+			},
+		},
+		production: {
+			approval: 2.1,
+		},
+		forged: {
+			fees: "1173040419815",
+			rewards: "42367000000000",
+			total: "43540040419815",
+		},
+	},
+	{
+		username: "ravelou",
+		address: "AXvBF7JoNyM6ztMrgr45KrqQf7LA7RgZhf",
+		publicKey:
+			"021f277f1e7a48c88f9c02988f06ca63d6f1781471f78dba49d58bab85eb3964c6",
+		votes: "229750415753086",
+		rank: 2,
+		isResigned: false,
+		blocks: {
+			produced: 224824,
+			last: {
+				id:
+					"69fa155007a2ee2e4cacd4b370b1de455a5c5e904403036747966513444ddd6c",
+				height: 11807768,
+				timestamp: {
+					epoch: 95638912,
+					unix: 1585740112,
+					human: "2020-04-01T11:21:52.000Z",
+				},
+			},
+		},
+		production: {
+			approval: 1.55,
+		},
+		forged: {
+			fees: "777973093653",
+			rewards: "44964800000000",
+			total: "45742773093653",
+		},
+	},
+	{
+		username: "dutchdelegate",
+		address: "AZse3vk8s3QEX1bqijFb21aSBeoF6vqLYE",
+		publicKey:
+			"0218b77efb312810c9a549e2cc658330fcc07f554d465673e08fa304fa59e67a0a",
+		votes: "218047638978442",
+		rank: 3,
+		isResigned: false,
+		blocks: {
+			produced: 195348,
+			last: {
+				id:
+					"ea7a1486e0605caf7133f33e988b97b9ec86e2508cc5daf6eebbbfe220e2bdf1",
+				height: 11807727,
+				timestamp: {
+					epoch: 95638584,
+					unix: 1585739784,
+					human: "2020-04-01T11:16:24.000Z",
+				},
+			},
+		},
+		production: {
+			approval: 1.47,
+		},
+		forged: {
+			fees: "607514657095",
+			rewards: "39069600000000",
+			total: "39677114657095",
+		},
+	},
+	{
+		username: "samuray",
+		address: "ARVNmpr14oh1X3Ee31TVF4vQr2QmfXvbwE",
+		publicKey:
+			"024bd465c9671f6fa6d5b0c3f7debd10618c2b7089dbf71c109f4fe02d1a03dea7",
+		votes: "204525536986774",
+		rank: 4,
+		isResigned: false,
+		blocks: {
+			produced: 224078,
+			last: {
+				id:
+					"4455845f1c0981f8503e77053bece8d201ae00d84f9682b2b76bb3d547cd71aa",
+				height: 11807733,
+				timestamp: {
+					epoch: 95638632,
+					unix: 1585739832,
+					human: "2020-04-01T11:17:12.000Z",
+				},
+			},
+		},
+		production: {
+			approval: 1.38,
+		},
+		forged: {
+			fees: "381807375399",
+			rewards: "44522600000000",
+			total: "44904407375399",
+		},
+	},
+	{
+		username: "biz_private",
+		address: "AaAy8BZkjV86YN7xUtZ35iwyXRMQKtKoAy",
+		publicKey:
+			"02fa6902e91e127d6d3410f6abc271a79ae24029079caa0db5819757e3c1c1c5a4",
+		votes: "197395013405329",
+		rank: 5,
+		isResigned: false,
+		blocks: {
+			produced: 185572,
+			last: {
+				id:
+					"5ab0813985c603d6648f6c7408b613697f1897b5b1caccfdccf5e39d4fec53b0",
+				height: 11807765,
+				timestamp: {
+					epoch: 95638888,
+					unix: 1585740088,
+					human: "2020-04-01T11:21:28.000Z",
+				},
+			},
+		},
+		production: {
+			approval: 1.33,
+		},
+		forged: {
+			fees: "2716264944121",
+			rewards: "37114400000000",
+			total: "39830664944121",
+		},
+	},
+	{
+		username: "kaos",
+		address: "ARKpUXE23A5NmWEAjb975Nj5yn4cbpYwUe",
+		publicKey:
+			"03c28f0c9d56bb30976830e75cce59b908fa1d2489d30a7e8ad494573deae23c20",
+		votes: "190269458433347",
+		rank: 6,
+		isResigned: false,
+		blocks: {
+			produced: 53288,
+			last: {
+				id:
+					"0d3d09dd471d204913b1614bfac73107de04b5951a7c7ebbe522db442e9b89e0",
+				height: 11807753,
+				timestamp: {
+					epoch: 95638792,
+					unix: 1585739992,
+					human: "2020-04-01T11:19:52.000Z",
+				},
+			},
+		},
+		production: {
+			approval: 1.28,
+		},
+		forged: {
+			fees: "22190969350",
+			rewards: "10657600000000",
+			total: "10679790969350",
+		},
+	},
+	{
+		username: "arky",
+		address: "ARfDVWZ7Zwkox3ZXtMQQY1HYSANMB88vWE",
+		publicKey:
+			"030da05984d579395ce276c0dd6ca0a60140a3c3d964423a04e7abe110d60a15e9",
+		votes: "189108458985617",
+		rank: 7,
+		isResigned: false,
+		blocks: {
+			produced: 227811,
+			last: {
+				id:
+					"20c1a1fc5010d6790a9a9268657587ec49de2da2c45b1ff7fa271e756cf37d60",
+				height: 11807771,
+				timestamp: {
+					epoch: 95638936,
+					unix: 1585740136,
+					human: "2020-04-01T11:22:16.000Z",
+				},
+			},
+		},
+		production: {
+			approval: 1.27,
+		},
+		forged: {
+			fees: "449450988586",
+			rewards: "45277800000000",
+			total: "45727250988586",
+		},
+	},
+	{
+		username: "calidelegate",
+		address: "AMvZYkTSMXCqFLPP4bxJyq84FfMg9m2V81",
+		publicKey:
+			"037b4730e7c63252912825510423487e9ee507e1a5719dfeef52187d8ababe3048",
+		votes: "186933293108945",
+		rank: 8,
+		isResigned: false,
+		blocks: {
+			produced: 191042,
+			last: {
+				id:
+					"fb66a77bf01b1e4c4b23d16784c1be1b50766919653e4f8962bed17c7107ff04",
+				height: 11807749,
+				timestamp: {
+					epoch: 95638760,
+					unix: 1585739960,
+					human: "2020-04-01T11:19:20.000Z",
+				},
+			},
+		},
+		production: {
+			approval: 1.26,
+		},
+		forged: {
+			fees: "331605218248",
+			rewards: "38208400000000",
+			total: "38540005218248",
+		},
+	},
+	{
+		username: "arkpool",
+		address: "ARAq9nhjCxwpWnGKDgxveAJSijNG8Y6dFQ",
+		publicKey:
+			"02b1d2ea7c265db66087789f571fceb8cc2b2d89e296ad966efb8ed51855f2ae0b",
+		votes: "186559717743128",
+		rank: 9,
+		isResigned: false,
+		blocks: {
+			produced: 231448,
+			last: {
+				id:
+					"1308b1dab6b83e8f44eb495990835ebb40f1d9bcdf61d3ec0cd403d0e51406d7",
+				height: 11807746,
+				timestamp: {
+					epoch: 95638736,
+					unix: 1585739936,
+					human: "2020-04-01T11:18:56.000Z",
+				},
+			},
+		},
+		production: {
+			approval: 1.26,
+		},
+		forged: {
+			fees: "535941108757",
+			rewards: "46121600000000",
+			total: "46657541108757",
+		},
+	},
+	{
+		username: "bioly",
+		address: "AbUdMhk96FbzxH7vDYAwdyqUELmLopZV5x",
+		publicKey:
+			"02c0b645f19ab304d25aae3add139edd9f6ca9fd0d98e57a808100de0e93832181",
+		votes: "179779979201241",
+		rank: 10,
+		isResigned: false,
+		blocks: {
+			produced: 231800,
+			last: {
+				id:
+					"50527cc332665d491b5d29933481bb5fb199dfb874fc58ac871c2cafedcfb2af",
+				height: 11807742,
+				timestamp: {
+					epoch: 95638704,
+					unix: 1585739904,
+					human: "2020-04-01T11:18:24.000Z",
+				},
+			},
+		},
+		production: {
+			approval: 1.21,
+		},
+		forged: {
+			fees: "453309577415",
+			rewards: "46084600000000",
+			total: "46537909577415",
+		},
+	},
+	{
+		username: "toad",
+		address: "AXW2vq7bNvAJS7uRcZBdyRWca9Dehtr8ds",
+		publicKey:
+			"031c110968941f29c76f3752d5752d5541cfe65f23f5d7f9115d2f4eb194f41c47",
+		votes: "179255306250340",
+		rank: 11,
+		isResigned: false,
+		blocks: {
+			produced: 219757,
+			last: {
+				id:
+					"592bd3e83f039b80c2616da694424bc9a84b17bdb3e6b36a258ffddc6262aef4",
+				height: 11807760,
+				timestamp: {
+					epoch: 95638848,
+					unix: 1585740048,
+					human: "2020-04-01T11:20:48.000Z",
+				},
+			},
+		},
+		production: {
+			approval: 1.21,
+		},
+		forged: {
+			fees: "364661172370",
+			rewards: "43664200000000",
+			total: "44028861172370",
+		},
+	},
+	{
+		username: "doc",
+		address: "AR2iXnLRp9HuoT584m6tjNWLHGWdX8uuGK",
+		publicKey:
+			"03cefbfa0c1c853084591b62a9aad0116029eaebdc27c2e3b811b1b0aebb928fc6",
+		votes: "176807710626837",
+		rank: 12,
+		isResigned: false,
+		blocks: {
+			produced: 222775,
+			last: {
+				id:
+					"3ceedb906c388c1e5d27a32d00de0a30b27f4e6b9ed7c3338a0ccfff74426445",
+				height: 11807691,
+				timestamp: {
+					epoch: 95638296,
+					unix: 1585739496,
+					human: "2020-04-01T11:11:36.000Z",
+				},
+			},
+		},
+		production: {
+			approval: 1.19,
+		},
+		forged: {
+			fees: "332215429251",
+			rewards: "44530200000000",
+			total: "44862415429251",
+		},
+	},
+	{
+		username: "geops",
+		address: "AU1TGCYoWzBnGMyMK9GTS8BG6EFbgc5xxC",
+		publicKey:
+			"02bf72c578a12c35a97ca1230b93017161ee42c3f0ab82f6fe7c95b3b43561a076",
+		votes: "175754041933245",
+		rank: 13,
+		isResigned: false,
+		blocks: {
+			produced: 115911,
+			last: {
+				id:
+					"451d366beb64ae967f43789fd06a87ab3d0a0bd207c34a05c908cab60118804b",
+				height: 11807757,
+				timestamp: {
+					epoch: 95638824,
+					unix: 1585740024,
+					human: "2020-04-01T11:20:24.000Z",
+				},
+			},
+		},
+		production: {
+			approval: 1.18,
+		},
+		forged: {
+			fees: "145902348118",
+			rewards: "23108200000000",
+			total: "23254102348118",
+		},
+	},
+	{
+		username: "barometer_delegate",
+		address: "AbARoTzauHZfczyJ1zgH2E7LogoNjCme4k",
+		publicKey:
+			"03cbf08dcbf09502ca015c99321f6fea8511d39c18684353feb9c3015825d7ec67",
+		votes: "172022309114101",
+		rank: 14,
+		isResigned: false,
+		blocks: {
+			produced: 18142,
+			last: {
+				id:
+					"1c722460be4b0c2ca3bd37eca177f60d8f8543b60fcca3470c32a87c32803f8b",
+				height: 11807729,
+				timestamp: {
+					epoch: 95638600,
+					unix: 1585739800,
+					human: "2020-04-01T11:16:40.000Z",
+				},
+			},
+		},
+		production: {
+			approval: 1.16,
+		},
+		forged: {
+			fees: "0",
+			rewards: "3628400000000",
+			total: "3628400000000",
+		},
+	},
+	{
+		username: "quarkpool",
+		address: "Abi3CE3k7XzYGtLjG7toZ5fF31stSmW55H",
+		publicKey:
+			"027c550ecc71e4d9b832cf83c1c5c4d7c22dc43e3a451fea887c82a6ef55bf52fa",
+		votes: "171372029049649",
+		rank: 15,
+		isResigned: false,
+		blocks: {
+			produced: 216144,
+			last: {
+				id:
+					"40a0bbbf9f6469d3e165b8e36ddb89687327bc156294d5d9536258f07bce8f13",
+				height: 11807726,
+				timestamp: {
+					epoch: 95638576,
+					unix: 1585739776,
+					human: "2020-04-01T11:16:16.000Z",
+				},
+			},
+		},
+		production: {
+			approval: 1.15,
+		},
+		forged: {
+			fees: "422664866146",
+			rewards: "43228800000000",
+			total: "43651464866146",
+		},
+	},
+	{
+		username: "goose",
+		address: "ALLZ3TQKTaHm2Bte4SrXL9C5cS8ZovqFfZ",
+		publicKey:
+			"03c5d32dedf5441b3aafb2e0c6ad3e5568bb0b3e822807b133e2276e014d830e3c",
+		votes: "165716568099782",
+		rank: 16,
+		isResigned: false,
+		blocks: {
+			produced: 193664,
+			last: {
+				id:
+					"142dc1e1e6a1a51b66391591df42fdf563048ebe215c83ecbba08c064c84d675",
+				height: 11807737,
+				timestamp: {
+					epoch: 95638664,
+					unix: 1585739864,
+					human: "2020-04-01T11:17:44.000Z",
+				},
+			},
+		},
+		production: {
+			approval: 1.12,
+		},
+		forged: {
+			fees: "937208618656",
+			rewards: "38732800000000",
+			total: "39670008618656",
+		},
+	},
+	{
+		username: "arkmoon",
+		address: "AKATy581uXWrbm8B4DTQh4R9RbqaWRiKRY",
+		publicKey:
+			"0232b96d57ac27f9a99242bc886e433baa89f596d435153c9dae47222c0d1cecc3",
+		votes: "161885493786160",
+		rank: 17,
+		isResigned: false,
+		blocks: {
+			produced: 186142,
+			last: {
+				id:
+					"443db263022d33119be411c04ec95c462a5c25d09d53b2452c00213241fa3a06",
+				height: 11807759,
+				timestamp: {
+					epoch: 95638840,
+					unix: 1585740040,
+					human: "2020-04-01T11:20:40.000Z",
+				},
+			},
+		},
+		production: {
+			approval: 1.09,
+		},
+		forged: {
+			fees: "363969318617",
+			rewards: "37228400000000",
+			total: "37592369318617",
+		},
+	},
+	{
+		username: "thefoundry",
+		address: "ALUeCYpPvPUMt9FUEWWf2xAoaX3WXo9hou",
+		publicKey:
+			"028f9f37a1fbf95bfa5b302032806116b27fd74996df7bcd6188c982f75e894ca4",
+		votes: "157778225631954",
+		rank: 18,
+		isResigned: false,
+		blocks: {
+			produced: 179190,
+			last: {
+				id:
+					"7b9a5aeca908241d328d8fadb22f8290753b6dde5c605f4301d28b9bfbb590a8",
+				height: 11807735,
+				timestamp: {
+					epoch: 95638648,
+					unix: 1585739848,
+					human: "2020-04-01T11:17:28.000Z",
+				},
+			},
+		},
+		production: {
+			approval: 1.06,
+		},
+		forged: {
+			fees: "943817549832",
+			rewards: "35838000000000",
+			total: "36781817549832",
+		},
+	},
+	{
+		username: "arkane",
+		address: "AZDc97RxUETWX3tPKEvWHWMNdrfN8CvEpR",
+		publicKey:
+			"03a28aeb6b15e792f1753caae663a52f15ab642ae7cdee6fcdbc5416ffc0f4f702",
+		votes: "156649171346467",
+		rank: 19,
+		isResigned: false,
+		blocks: {
+			produced: 175769,
+			last: {
+				id:
+					"5fad90c1873aaa80dc0b75a1f01433f80ef3af22b71ed1169dc32900a44f34d7",
+				height: 11807754,
+				timestamp: {
+					epoch: 95638800,
+					unix: 1585740000,
+					human: "2020-04-01T11:20:00.000Z",
+				},
+			},
+		},
+		production: {
+			approval: 1.06,
+		},
+		forged: {
+			fees: "243385123136",
+			rewards: "35153800000000",
+			total: "35397185123136",
+		},
+	},
+	{
+		username: "blues_for_alice",
+		address: "ALuQkdsJQKypBA6XCzK84b5v5RpXrfgPYK",
+		publicKey:
+			"03c7219fa2d51bee35ce6ad232bb430d9760a2013549eb9365f6348f69ac3f7802",
+		votes: "155726925666666",
+		rank: 20,
+		isResigned: false,
+		blocks: {
+			produced: 15143,
+			last: {
+				id:
+					"d65a9f8b33a1d620442c5f0bee813551224dab4d5cbfb3f8dcaa4cfe704d5431",
+				height: 11807731,
+				timestamp: {
+					epoch: 95638616,
+					unix: 1585739816,
+					human: "2020-04-01T11:16:56.000Z",
+				},
+			},
+		},
+		production: {
+			approval: 1.05,
+		},
+		forged: {
+			fees: "5465978816",
+			rewards: "3028600000000",
+			total: "3034065978816",
+		},
+	},
+];
