a11b25723106b4f98ce172abf0004b61c1ae2832
Caio Alcântara <alcantaracaiolucas@gmail.com>
2020-04-14 10:41:08 -0300

    feat: add status component (#564)
    * feat: add status component
    
    * chore: remove single test run flag
    
    * feat: add warning and error variations, apply svg
    
    * test: update test cases to new component structure
    
    * feat: update component usage
    
    * refactor: apply review suggestions

---
+++ src/app/components/alert/alert.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from "@angular/core";
+import { IonicModule } from "@ionic/angular";
+
+import { SharedModule } from "@/app/shared.module";
+import { DirectivesModule } from "@/directives/directives.module";
+
+import { Alert } from "./alert.component";
+
+@NgModule({
+	declarations: [Alert],
+	imports: [IonicModule, SharedModule, DirectivesModule],
+	exports: [Alert],
+})
+export class AlertModule {}
