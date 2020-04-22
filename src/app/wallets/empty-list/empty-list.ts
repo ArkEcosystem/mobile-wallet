e7a2509e98c7d8e64f0b7950282c444e3add56f9
Caio Alcantara <alcantaracaiolucas@gmail.com>
2020-04-22 13:56:50 -0300

    feat(empty-list): add emitter and click handlers

--- src/app/wallets/empty-list/empty-list.ts
+++ src/app/wallets/empty-list/empty-list.ts
@@ -1,10 +1,4 @@
-import { Component, Input } from "@angular/core";
-import {
-	ActionSheetController,
-	ModalController,
-	NavController,
-} from "@ionic/angular";
-import { TranslateService } from "@ngx-translate/core";
+import { Component, EventEmitter, Input, Output } from "@angular/core";
 
 @Component({
 	selector: "empty-list",
@@ -18,63 +12,19 @@ export class WalletEmptyListComponent {
 	@Input()
 	name: string;
 
-	constructor(
-		public navCtrl: NavController,
-		private modalCtrl: ModalController,
-		private actionSheetCtrl: ActionSheetController,
-		private translateService: TranslateService,
-	) {}
+	@Output()
+	importWalletClick = new EventEmitter();
 
-	presentWalletImport(role: string) {
-		if (role === "qrcode") {
-			this.navCtrl.navigateForward("/wallets/import");
-		} else {
-			this.navCtrl.navigateForward("/wallets/import-manual", {
-				queryParams: {
-					type: role,
-				},
-			});
-		}
+	@Output()
+	generateWalletClick = new EventEmitter();
+
+	constructor() {}
+
+	public importWalletHandler() {
+		this.importWalletClick.emit();
 	}
 
-	presentActionSheet() {
-		this.translateService
-			.get([
-				"WALLETS_PAGE.SCAN_QRCODE",
-				"IMPORT_PASSPHRASE",
-				"IMPORT_ADDRESS",
-			])
-			.subscribe(async (translation) => {
-				const actionSheet = await this.actionSheetCtrl.create({
-					buttons: [
-						{
-							text: translation["WALLETS_PAGE.SCAN_QRCODE"],
-							role: "qrcode",
-							icon: "qr-code",
-							handler: () => {
-								this.presentWalletImport("qrcode");
-							},
-						},
-						{
-							text: translation.IMPORT_PASSPHRASE,
-							role: "passphrase",
-							icon: "lock-closed",
-							handler: () => {
-								this.presentWalletImport("passphrase");
-							},
-						},
-						{
-							text: translation.IMPORT_ADDRESS,
-							role: "address",
-							icon: "globe",
-							handler: () => {
-								this.presentWalletImport("address");
-							},
-						},
-					],
-				});
-
-				actionSheet.present();
-			});
+	public generateWalletHandler() {
+		this.generateWalletClick.emit();
 	}
 }
