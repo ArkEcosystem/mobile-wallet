682f17ee5144f4f762a50a0a48ad56839514deda
Caio Alcantara <alcantaracaiolucas@gmail.com>
2020-04-27 16:39:23 -0300

    chore: remove ununsed output

--- src/app/networks/network-list/network-list.component.ts
+++ src/app/networks/network-list/network-list.component.ts
@@ -1,19 +1,12 @@
-import { Component, EventEmitter, Input, Output } from "@angular/core";
+import { Component, Input } from "@angular/core";
 
 @Component({
 	selector: "network-list",
 	templateUrl: "network-list.component.html",
 })
 export class NetworkListComponent {
-	@Output()
-	public addNetworkHandler = new EventEmitter();
-
 	@Input()
 	public networks = [];
 
 	constructor() {}
-
-	public handleAddNetwork() {
-		this.addNetworkHandler.emit();
-	}
 }
