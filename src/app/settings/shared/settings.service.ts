28aa4134ba0c13a377fab46f7910c2dcfdd78b38
Caio Alcantara <alcantaracaiolucas@gmail.com>
2020-04-28 11:58:03 -0300

    refactor(service): update storage service

--- src/app/settings/shared/settings.service.ts
+++ src/app/settings/shared/settings.service.ts
@@ -1,24 +1,20 @@
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 
-import { StorageService } from "@/app/core/storage.service";
+import { AsyncStorageService } from "@/services/storage/async-storage.service";
 
 import { SettingsConfig } from "../settings.config";
 import { SettingsStateModel } from "./settings.type";
 
 @Injectable()
 export class SettingsService {
-	constructor(private storageService: StorageService) {}
-
-	public save(payload: SettingsStateModel): Observable<void> {
-		return this.storageService.set(SettingsConfig.TOKEN, payload);
-	}
+	constructor(private asyncStorage: AsyncStorageService) {}
 
 	public load(): Observable<Partial<SettingsStateModel>> {
-		return this.storageService.getObject(SettingsConfig.TOKEN);
+		return this.asyncStorage.getItem(SettingsConfig.TOKEN);
 	}
 
 	public clear(): Observable<void> {
-		return this.storageService.clear();
+		return this.asyncStorage.removeItem(SettingsConfig.TOKEN);
 	}
 }
