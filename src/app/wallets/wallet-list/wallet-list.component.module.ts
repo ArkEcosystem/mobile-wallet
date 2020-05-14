5bb153b606ebf0f8bfb73488f4f8533c38cd4c3a
Caio Alcantara <alcantaracaiolucas@gmail.com>
2020-05-14 15:14:15 -0300

    fix: fix e2e job

--- src/app/wallets/wallet-list/wallet-list.component.module.ts
+++ src/app/wallets/wallet-list/wallet-list.component.module.ts
@@ -3,8 +3,14 @@ import { NgModule } from "@angular/core";
 import { IonicModule } from "@ionic/angular";
 import { TranslateModule } from "@ngx-translate/core";
 
+import { GenerateEntropyModal } from "@/app/modals/generate-entropy/generate-entropy";
+import { GenerateEntropyModalModule } from "@/app/modals/generate-entropy/generate-entropy.module";
+import { WalletBackupModal } from "@/app/modals/wallet-backup/wallet-backup";
+import { WalletBackupModalModule } from "@/app/modals/wallet-backup/wallet-backup.module";
 import { PipesModule } from "@/pipes/pipes.module";
 
+import { EnterSecondPassphraseModal } from "../../modals/enter-second-passphrase/enter-second-passphrase";
+import { EnterSecondPassphraseModalModule } from "../../modals/enter-second-passphrase/enter-second-passphrase.module";
 import { WalletCardComponentModule } from "../wallet-card/wallet-card.component.module";
 import { WalletListActionsComponentModule } from "../wallet-list-actions/wallet-list-actions.component.module";
 import { WalletListEmptyComponentModule } from "../wallet-list-empty/wallet-list-empty.component.module";
@@ -18,11 +24,19 @@ import { WalletListComponent } from "./wallet-list.component";
 		CommonModule,
 		TranslateModule,
 		PipesModule,
+		GenerateEntropyModalModule,
+		EnterSecondPassphraseModalModule,
+		WalletBackupModalModule,
 		WalletListEmptyComponentModule,
 		WalletListActionsComponentModule,
 		WalletListHeaderComponentModule,
 		WalletCardComponentModule,
 	],
 	exports: [WalletListComponent],
+	entryComponents: [
+		GenerateEntropyModal,
+		WalletBackupModal,
+		EnterSecondPassphraseModal,
+	],
 })
 export class WalletListComponentModule {}
