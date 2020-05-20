6a3e7ee59143ce2ab8605c9a77da19bcbf5b1f8b
Caio Alcantara <alcantaracaiolucas@gmail.com>
2020-05-20 10:02:39 -0300

    feat: add copy to clipboard action

--- src/app/password/password-page.spec.ts
+++ src/app/password/password-page.spec.ts
@@ -1,9 +1,11 @@
 import { APP_BASE_HREF } from "@angular/common";
 import { RouterTestingModule } from "@angular/router/testing";
+import { Clipboard } from "@ionic-native/clipboard/ngx";
 import { NavController } from "@ionic/angular";
 import {
 	byTestId,
 	createHostComponentFactory,
+	createSpyObject,
 	SpectatorHost,
 } from "@ngneat/spectator";
 import { TranslateModule } from "@ngx-translate/core";
@@ -12,40 +14,47 @@ import { PipesModule } from "@/pipes/pipes.module";
 
 import { PasswordPageComponent } from "./password-page.component";
 
-const navMock = {
-	navigateForward: (path) => path,
-};
-
 describe("Password Page", () => {
 	let spectator: SpectatorHost<PasswordPageComponent>;
+	const clipboardProvider = {
+		provide: Clipboard,
+		useValue: createSpyObject(Clipboard),
+	};
+	const navMock = {
+		navigateForward: (path) => path,
+	};
 	const createHost = createHostComponentFactory({
 		component: PasswordPageComponent,
 		imports: [RouterTestingModule, TranslateModule.forRoot(), PipesModule],
+		componentProviders: [clipboardProvider],
 		providers: [
+			clipboardProvider,
 			{ provide: APP_BASE_HREF, useValue: "/" },
 			{ provide: NavController, useValue: navMock },
 		],
 	});
+
+	const words = [
+		"blame",
+		"fire",
+		"duck",
+		"blame",
+		"fire",
+		"duck",
+		"blame",
+		"fire",
+		"duck",
+		"blame",
+		"fire",
+		"duck",
+	];
 
 	it("should create", () => {
 		spectator = createHost(
 			`<password-page [words]="words"></password-page>`,
 			{
 				hostProps: {
-					words: [
-						"blame",
-						"fire",
-						"duck",
-						"blame",
-						"fire",
-						"duck",
-						"blame",
-						"fire",
-						"duck",
-						"blame",
-						"fire",
-						"duck",
-					],
+					words,
 				},
 			},
 		);
@@ -58,20 +67,7 @@ describe("Password Page", () => {
 			`<password-page [words]="words"></password-page>`,
 			{
 				hostProps: {
-					words: [
-						"blame",
-						"fire",
-						"duck",
-						"blame",
-						"fire",
-						"duck",
-						"blame",
-						"fire",
-						"duck",
-						"blame",
-						"fire",
-						"duck",
-					],
+					words,
 				},
 			},
 		);
@@ -84,5 +80,25 @@ describe("Password Page", () => {
 		spectator.click(nextStepButton);
 
 		expect(navCtrl.navigateForward).toHaveBeenCalledWith("/password/check");
+	});
+
+	it("should copy words to clipboard", () => {
+		spectator = createHost(
+			`<password-page [words]="words"></password-page>`,
+			{
+				hostProps: {
+					words,
+				},
+			},
+		);
+		const clipboard = spectator.get(Clipboard);
+		clipboard.copy.and.resolveTo();
+
+		const copyButton = spectator.query(
+			byTestId("password_page__actions--button-group--copy"),
+		);
+		spectator.click(copyButton);
+
+		expect(clipboard.copy).toHaveBeenCalledWith(words.join(" "));
 	});
 });
