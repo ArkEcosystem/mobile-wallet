b50573282edf229c8423dd34064eaa808e848651
Caio Alcantara <alcantaracaiolucas@gmail.com>
2020-05-21 09:11:27 -0300

    feat: trade functions for built in usage, adjust BEM

--- src/app/transactions/transaction-row/transaction-row.component.ts
+++ src/app/transactions/transaction-row/transaction-row.component.ts
@@ -14,34 +14,6 @@ export class TransactionRowComponent {
 
 	constructor() {}
 
-	public truncate(str) {
-		const separator = "...";
-		const max = 14;
-		const separatorLength = separator.length;
-
-		const shift = -0.5 * (max - length - separatorLength);
-		const center = length / 2;
-
-		return (
-			str.substr(0, center - shift) +
-			separator +
-			str.substr(length - center + shift)
-		);
-	}
-
-	public getTransactionTime(timestamp: string) {
-		const date = new Date(parseInt(timestamp) * 1000);
-		const hours = date.getHours();
-		const minutes = date.getMinutes();
-		const seconds = date.getSeconds();
-
-		return `${hours}:${minutes}:${seconds}`;
-	}
-
-	public getTransactionLabel(type: string) {
-		return type === "sent" ? "Sent to" : "From";
-	}
-
 	public openTransactionDetailsHandler(id: string) {
 		this.openTransactionDetails.emit(id);
 	}
