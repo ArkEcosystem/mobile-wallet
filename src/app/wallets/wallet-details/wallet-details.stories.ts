b1b02c11adbcd9e9d580963c86e920886e6476c9
Caio Alcantara <alcantaracaiolucas@gmail.com>
2020-06-02 04:41:28 -0300

    chore: update stories

--- src/app/wallets/wallet-details/wallet-details.stories.ts
+++ src/app/wallets/wallet-details/wallet-details.stories.ts
@@ -3,10 +3,14 @@ import { IonicModule } from "@ionic/angular";
 import { select, text, withKnobs } from "@storybook/addon-knobs";
 import { moduleMetadata } from "@storybook/angular";
 
+// Fixtures
+import { newTransactions } from "@@/test/fixture/transactions.fixture";
+// Modules
 import { BottomDrawerComponentModule } from "@/components/bottom-drawer/bottom-drawer.module";
 import { IdenticonComponentModule } from "@/components/identicon/identicon.module";
 import { PipesModule } from "@/pipes/pipes.module";
 
+// UI Elements
 import { TransactionListComponentModule } from "../../transactions/transaction-list/transaction-list.component.module";
 import { WalletDetailsComponent } from "./wallet-details.component";
 
@@ -33,11 +37,41 @@ export const WalletDetails = () => ({
 		address: text("Address", "AHJJ29sCdR5UNZjdz3BYeDpvvkZCGBjde9"),
 		balance: text("Balance", "20000"),
 		currency: select("Coin", { ARK: "ARK", Bitcoin: "BTC" }, "ARK"),
+		transactions: newTransactions,
 	},
 	template: `
 			<ion-app>
 				<ion-content class="ion-padding w-full flex justify-center items-center">
-					<wallet-details [balance]="balance" [currency]="currency" [address]="address" ></wallet-details>
+					<wallet-details
+						[balance]="balance"
+						[currency]="currency"
+						[address]="address"
+						[transactions]="transactions"
+					>
+					</wallet-details>
+				</ion-content>
+			</ion-app>
+		`,
+});
+
+export const WalletDetailsEmpty = () => ({
+	component: WalletDetailsComponent,
+	props: {
+		address: text("Address", "AHJJ29sCdR5UNZjdz3BYeDpvvkZCGBjde9"),
+		balance: text("Balance", "20000"),
+		currency: select("Coin", { ARK: "ARK", Bitcoin: "BTC" }, "ARK"),
+		transactions: [],
+	},
+	template: `
+			<ion-app>
+				<ion-content class="ion-padding w-full flex justify-center items-center">
+					<wallet-details
+						[balance]="balance"
+						[currency]="currency"
+						[address]="address"
+						[transactions]="transactions"
+					>
+					</wallet-details>
 				</ion-content>
 			</ion-app>
 		`,
@@ -45,4 +79,8 @@ export const WalletDetails = () => ({
 
 WalletDetails.story = {
 	name: "wallet-details",
+};
+
+WalletDetailsEmpty.story = {
+	name: "wallet-details transactions empty list",
 };
