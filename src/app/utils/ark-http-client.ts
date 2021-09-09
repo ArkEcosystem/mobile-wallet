9ca9ed8b4417cef3ef65d8b15a2482b16dc73dfa
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2021-09-09 22:04:45 +0100

    wip: throw caught errors while logging, remove commented code, and attempt to force a json content type

--- src/app/utils/ark-http-client.ts
+++ src/app/utils/ark-http-client.ts
@@ -1,4 +1,3 @@
-// import crossFetch from 'cross-fetch';
 import { Injectable } from "@angular/core";
 import { HTTP } from "@ionic-native/http/ngx";
 import { from, Observable } from "rxjs";
@@ -8,114 +7,80 @@ import { catchError, map } from "rxjs/operators";
 	providedIn: "root",
 })
 export class HttpClient {
-	// private static http: HTTP;
+	private defaultHeaders: any = {
+		"Content-Type": "application/json",
+	};
 
-	// private client: any;
-
-	// constructor(host: string, httpClient: HttpClient) {
-	// 	this.client = ;
-	// }
-
-	constructor(private http: HTTP) {}
-	// constructor() {
-	// 	console.log('asdasdsad');
-	// }
+	constructor(private http: HTTP) {
+		this.http.setDataSerializer("json");
+	}
 
 	get<T>(url: string, options: any = {}): Observable<any> {
 		console.log("HttpClient#get", url);
 
-		return from(this.http.get(url, {}, options.headers ?? {}))
-			.pipe(
-				map(function (result): any {
-					console.log(
-						"HttpClient#get result",
-						result,
-						JSON.parse(result.data),
-					);
+		return from(
+			this.http.get(url, {}, options.headers ?? this.defaultHeaders),
+		).pipe(
+			map(function (result): any {
+				console.log(
+					"HttpClient#get result",
+					result,
+					JSON.parse(result.data),
+				);
 
-					return JSON.parse(result.data);
-				}),
-			)
-			.pipe(
-				catchError(function (error): any {
-					console.log("HttpClient#get error", error);
-				}),
-			);
-		// return this.request('GET', url);
+				return JSON.parse(result.data);
+			}),
+			catchError(function (error): any {
+				console.log("HttpClient#get error", error);
+
+				throw error;
+			}),
+		);
 	}
 
 	post<T>(url: string, body: any = {}, options: any = {}): Observable<any> {
 		console.log("HttpClient#post", url, body);
 
-		return from(this.http.post(url, body, options.headers ?? {}))
-			.pipe(
-				map(function (result): any {
-					console.log(
-						"HttpClient#post result",
-						result,
-						JSON.parse(result.data),
-					);
+		return from(
+			this.http.post(url, body, options.headers ?? this.defaultHeaders),
+		).pipe(
+			map(function (result): any {
+				console.log(
+					"HttpClient#post result",
+					result,
+					JSON.parse(result.data),
+				);
 
-					return JSON.parse(result.data);
-				}),
-			)
-			.pipe(
-				catchError(function (error): any {
-					console.log("HttpClient#post error", error);
-				}),
-			);
+				return JSON.parse(result.data);
+			}),
+			catchError(function (error): any {
+				console.log("HttpClient#post error", error);
+
+				throw error;
+			}),
+		);
 	}
 
 	put<T>(url: string, body: any = {}, options: any = {}): Observable<any> {
 		console.log("HttpClient#put", url, body);
 
-		return from(this.http.put(url, body, options.headers ?? {}))
-			.pipe(
-				map(function (result): any {
-					console.log(
-						"HttpClient#put result",
-						result,
-						JSON.parse(result.data),
-					);
+		return from(
+			this.http.put(url, body, options.headers ?? this.defaultHeaders),
+		).pipe(
+			map(function (result): any {
+				console.log(
+					"HttpClient#put result",
+					result,
+					JSON.parse(result.data),
+				);
 
-					return JSON.parse(result.data);
-				}),
-			)
-			.pipe(
-				catchError(function (error): any {
-					console.log("HttpClient#put error", error);
-				}),
-			);
+				return JSON.parse(result.data);
+			}),
+			catchError(function (error): any {
+				console.log("HttpClient#put error", error);
+
+				throw error;
+			}),
+		);
 	}
-
-	// TODO sort our body for post/put
-	// request<T>(method: string, url: string, options: any = {}): Observable<any> {
-	// 	console.log('HttpClient#request', method, url);
-
-	// 	return from(this.http[method.toLowerCase()](url, {
-	// 		method,
-	// 		headers: {
-	// 			...(options.headers ?? {}),
-	// 			'Content-Type': 'application/json',
-	// 		},
-	// 	}))
-	// 		.pipe(map(function(result): any {
-	// 			console.log('HttpClient#request result', result, JSON.parse((result as HTTPResponse).data));
-
-	// 			return JSON.parse((result as HTTPResponse).data);
-	// 		}))
-	// 		.pipe(catchError(function(error): any {
-	// 			console.log('HttpClient#request error', error);
-	// 		}));
-	// }
-
-	// request(method: string, url: string, headers?: any): Observable<any> {
-	// 	return from(this.http.(method)(url, {
-	// 		method,
-	// 		headers: {
-	// 			...headers,
-	// 			'Content-Type': 'application/json',
-	// 		},
-	// 	}));
-	// }
 }
