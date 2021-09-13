f2783f0771d660274e50636ec8e380f4aa06236e
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2021-09-13 22:02:05 +0100

    test: updates (wip)

--- src/app/services/ark-api/ark-api.spec.ts
+++ src/app/services/ark-api/ark-api.spec.ts
@@ -1,7 +1,6 @@
 import { HTTP } from "@ionic-native/http/ngx";
 import {
 	createHttpFactory,
-	HttpMethod,
 	mockProvider,
 	SpectatorHttp,
 } from "@ngneat/spectator";
@@ -9,13 +8,19 @@ import { TranslateService } from "@ngx-translate/core";
 import { of, Subject } from "rxjs";
 
 import delegatesFixture from "@@/test/fixture/delegates.fixture";
+import * as profilesFixtures from "@@/test/fixture/profiles.fixture";
 import feesFixture from "@@/test/fixture/transactions-fees.fixture";
-import { STORAGE_DELEGATES, STORAGE_FEES } from "@/app/app.constants";
+import {
+	STORAGE_DELEGATES,
+	STORAGE_FEES,
+	STORAGE_PROFILES,
+} from "@/app/app.constants";
 import { FeeStatistic, StoredNetwork } from "@/models/model";
 import { NetworkProvider } from "@/services/network/network";
 import { StorageProvider } from "@/services/storage/storage";
 import { ToastProvider } from "@/services/toast/toast";
 import { UserDataService } from "@/services/user-data/user-data.interface";
+import { HttpClient } from "@/utils/ark-http-client";
 
 import ArkClient from "../../utils/ark-client";
 import { ArkApiProvider } from "./ark-api";
@@ -52,6 +57,8 @@ describe("ARK API", () => {
 			mockProvider(StorageProvider, {
 				getObject: (prop: string) => {
 					switch (prop) {
+						case STORAGE_PROFILES:
+							return of(profilesFixtures);
 						case STORAGE_DELEGATES:
 							return of(delegatesFixture);
 						case STORAGE_FEES:
@@ -63,30 +70,30 @@ describe("ARK API", () => {
 		],
 	});
 
-	beforeEach(() => {
+	beforeEach(function () {
 		arkApiSpectator = createArkApiMock();
 		arkApiService = arkApiSpectator.service;
 	});
 
 	it("should get the current network", () => {
+		const spy = spyOn(HttpClient.prototype, "get" as any).and.callThrough();
+
 		const userDataService = arkApiSpectator.get(UserDataService);
 		userDataService.onActivateNetwork$.next(currentNetwork);
-		arkApiSpectator.expectConcurrent([
-			{
-				url: "http://127.0.0.1:4003/api/peers",
-				method: HttpMethod.GET,
-			},
-			{
-				url: "http://127.0.0.1:4003/api/transactions/fees",
-				method: HttpMethod.GET,
-			},
-		]);
+
+		expect(spy.calls.argsFor(0)[0]).toEqual(
+			"http://127.0.0.1:4003/api/peers",
+		);
+		expect(spy.calls.argsFor(1)[0]).toEqual(
+			"http://127.0.0.1:4003/api/transactions/fees",
+		);
 		expect(arkApiService.network.name).toEqual("custom");
 	});
 
 	it("should return if no network specified", () => {
 		const userDataService = arkApiSpectator.get(UserDataService);
 		userDataService.onActivateNetwork$.next();
+
 		expect(arkApiService.network).toEqual(undefined);
 	});
 
@@ -95,69 +102,59 @@ describe("ARK API", () => {
 	});
 
 	it("should return the transaction builder", () => {
+		const spy = spyOn(HttpClient.prototype, "get" as any).and.callThrough();
+
 		const userDataService = arkApiSpectator.get(UserDataService);
 		userDataService.onActivateNetwork$.next(currentNetwork);
-		arkApiSpectator.expectConcurrent([
-			{
-				url: "http://127.0.0.1:4003/api/peers",
-				method: HttpMethod.GET,
-			},
-			{
-				url: "http://127.0.0.1:4003/api/transactions/fees",
-				method: HttpMethod.GET,
-			},
-		]);
+
+		expect(spy.calls.argsFor(0)[0]).toEqual(
+			"http://127.0.0.1:4003/api/peers",
+		);
+		expect(spy.calls.argsFor(1)[0]).toEqual(
+			"http://127.0.0.1:4003/api/transactions/fees",
+		);
 		expect(arkApiService.transactionBuilder).not.toEqual(null);
 	});
 
-	it("should return empty if no network to the fee statistics", (done) => {
+	it("should return empty if no network to the fee statistics", async () => {
+		const spy = spyOn(HttpClient.prototype, "get" as any).and.callThrough();
+
 		const userDataService = arkApiSpectator.get(UserDataService);
 		userDataService.onActivateNetwork$.next(currentNetwork);
-		arkApiSpectator.expectConcurrent([
-			{
-				url: "http://127.0.0.1:4003/api/peers",
-				method: HttpMethod.GET,
-			},
-			{
-				url: "http://127.0.0.1:4003/api/transactions/fees",
-				method: HttpMethod.GET,
-			},
-		]);
-		arkApiService.feeStatistics.subscribe({
-			complete: () => done(),
-		});
+
+		expect(spy.calls.argsFor(0)[0]).toEqual(
+			"http://127.0.0.1:4003/api/peers",
+		);
+		expect(spy.calls.argsFor(1)[0]).toEqual(
+			"http://127.0.0.1:4003/api/transactions/fees",
+		);
+
+		await arkApiService.feeStatistics.toPromise();
 	});
 
-	it("should fetch network fee statistics", (done) => {
+	it("should fetch network fee statistics", async () => {
+		const spy = spyOn(HttpClient.prototype, "get" as any).and.callThrough();
+
 		const userDataService = arkApiSpectator.get(UserDataService);
 		currentNetwork.isV2 = true;
 		userDataService.onActivateNetwork$.next(currentNetwork);
 
-		arkApiService.feeStatistics.subscribe({
-			complete: () => done(),
-		});
+		await arkApiService.feeStatistics.toPromise();
 
-		const reqs = arkApiSpectator.expectConcurrent([
-			{
-				url: "http://127.0.0.1:4003/api/peers",
-				method: HttpMethod.GET,
-			},
-			{
-				url: "http://127.0.0.1:4003/api/transactions/fees",
-				method: HttpMethod.GET,
-			},
-			{
-				url: "http://127.0.0.1:4003/api/node/fees?days=7",
-				method: HttpMethod.GET,
-			},
-		]);
-
-		reqs[2].flush({ data: [] });
-
-		done();
+		expect(spy.calls.argsFor(0)[0]).toEqual(
+			"http://127.0.0.1:4003/api/peers",
+		);
+		expect(spy.calls.argsFor(1)[0]).toEqual(
+			"http://127.0.0.1:4003/api/transactions/fees",
+		);
+		expect(spy.calls.argsFor(2)[0]).toEqual(
+			"http://127.0.0.1:4003/api/node/fees?days=7",
+		);
 	});
 
-	it("should return network fee statistics", (done) => {
+	it("should return network fee statistics", async () => {
+		const spy = spyOn(HttpClient.prototype, "get" as any).and.callThrough();
+
 		const userDataService = arkApiSpectator.get(UserDataService);
 		// Mock FeeStatistic
 		const networkFeeStatistic: FeeStatistic = {
@@ -171,160 +168,110 @@ describe("ARK API", () => {
 		currentNetwork.feeStatistics = [networkFeeStatistic];
 		userDataService.onActivateNetwork$.next(currentNetwork);
 
-		arkApiService.feeStatistics.subscribe({
-			next: (data) => {
-				expect(data).toEqual([networkFeeStatistic]);
-				done();
-			},
-		});
-
-		arkApiSpectator.expectConcurrent([
-			{
-				url: "http://127.0.0.1:4003/api/peers",
-				method: HttpMethod.GET,
-			},
-			{
-				url: "http://127.0.0.1:4003/api/transactions/fees",
-				method: HttpMethod.GET,
-			},
+		expect(await arkApiService.feeStatistics.toPromise()).toEqual([
+			networkFeeStatistic,
 		]);
+		expect(spy.calls.argsFor(0)[0]).toEqual(
+			"http://127.0.0.1:4003/api/peers",
+		);
+		expect(spy.calls.argsFor(1)[0]).toEqual(
+			"http://127.0.0.1:4003/api/transactions/fees",
+		);
 	});
 
-	it("should fetch and return fees", (done) => {
+	it("should fetch and return fees", async () => {
+		const spy = spyOn(HttpClient.prototype, "get" as any).and.callThrough();
+
 		const userDataService = arkApiSpectator.get(UserDataService);
 		userDataService.onActivateNetwork$.next(currentNetwork);
 
-		arkApiSpectator.expectConcurrent([
-			{
-				url: "http://127.0.0.1:4003/api/peers",
-				method: HttpMethod.GET,
-			},
-			{
-				url: "http://127.0.0.1:4003/api/transactions/fees",
-				method: HttpMethod.GET,
-			},
-		]);
-
-		arkApiService.fees.subscribe({
-			next: (data) => {
-				expect(data).not.toEqual(null);
-				done();
-			},
-		});
-
-		const req = arkApiSpectator.expectOne(
-			"http://127.0.0.1:4003/api/transactions/fees",
-			HttpMethod.GET,
+		expect(spy.calls.argsFor(0)[0]).toEqual(
+			"http://127.0.0.1:4003/api/peers",
 		);
-
-		req.flush(feesFixture);
+		expect(spy.calls.argsFor(1)[0]).toEqual(
+			"http://127.0.0.1:4003/api/transactions/fees",
+		);
+		expect(await arkApiService.fees.toPromise()).not.toEqual(null);
+		expect(spy.calls.argsFor(2)[0]).toEqual(
+			"http://127.0.0.1:4003/api/transactions/fees",
+		);
 	});
 
-	it("should return cached fees", (done) => {
+	it("should return cached fees", async () => {
+		const spy = spyOn(HttpClient.prototype, "get" as any).and.callThrough();
+
 		const userDataService = arkApiSpectator.get(UserDataService);
 		// First call to trigger fetchFees
 		userDataService.onActivateNetwork$.next(currentNetwork);
 
-		const reqs = arkApiSpectator.expectConcurrent([
-			{
-				url: "http://127.0.0.1:4003/api/peers",
-				method: HttpMethod.GET,
-			},
-			{
-				url: "http://127.0.0.1:4003/api/transactions/fees",
-				method: HttpMethod.GET,
-			},
-		]);
-
-		reqs[1].flush(feesFixture);
+		expect(spy.calls.argsFor(0)[0]).toEqual(
+			"http://127.0.0.1:4003/api/peers",
+		);
+		expect(spy.calls.argsFor(1)[0]).toEqual(
+			"http://127.0.0.1:4003/api/transactions/fees",
+		);
 
 		// Should be using existent value instead of fetch again
-		arkApiService.fees.subscribe({
-			next: (data) => {
-				expect(data).not.toEqual(null);
-				done();
-			},
-		});
+		expect(await arkApiService.fees.toPromise()).not.toEqual(null);
 	});
 
-	xit("should fetch and return delegates", (done) => {
+	xit("should fetch and return delegates", async () => {
+		const spy = spyOn(HttpClient.prototype, "get" as any).and.callThrough();
+
 		const userDataService = arkApiSpectator.get(UserDataService);
 		currentNetwork.activeDelegates = 1;
 		userDataService.onActivateNetwork$.next(currentNetwork);
 
-		arkApiSpectator.expectConcurrent([
-			{
-				url: "http://127.0.0.1:4003/api/peers",
-				method: HttpMethod.GET,
-			},
-			{
-				url: "http://127.0.0.1:4003/api/transactions/fees",
-				method: HttpMethod.GET,
-			},
-		]);
-
-		arkApiService.delegates.subscribe({
-			next: (data) => {
-				expect(data).not.toEqual(null);
-				done();
-			},
-		});
-
-		const req = arkApiSpectator.expectOne(
-			"http://127.0.0.1:4003/api/delegates?limit=51&page=1",
-			HttpMethod.GET,
+		expect(spy.calls.argsFor(0)[0]).toEqual(
+			"http://127.0.0.1:4003/api/peers",
 		);
-		req.flush(delegatesFixture);
+		expect(spy.calls.argsFor(1)[0]).toEqual(
+			"http://127.0.0.1:4003/api/transactions/fees",
+		);
+		expect(await arkApiService.delegates.toPromise()).not.toEqual(null);
+		expect(spy.calls.argsFor(2)[0]).toEqual(
+			"http://127.0.0.1:4003/api/delegates?limit=51&page=1",
+		);
 	});
 
 	it("should validate an address", () => {
+		const spy = spyOn(HttpClient.prototype, "get" as any).and.callThrough();
+
 		// Set version and activate network
 		const userDataService = arkApiSpectator.get(UserDataService);
 		currentNetwork.version = 23;
 		userDataService.onActivateNetwork$.next(currentNetwork);
 
-		arkApiSpectator.expectConcurrent([
-			{
-				url: "http://127.0.0.1:4003/api/peers",
-				method: HttpMethod.GET,
-			},
-			{
-				url: "http://127.0.0.1:4003/api/transactions/fees",
-				method: HttpMethod.GET,
-			},
-		]);
-
+		expect(spy.calls.argsFor(0)[0]).toEqual(
+			"http://127.0.0.1:4003/api/peers",
+		);
+		expect(spy.calls.argsFor(1)[0]).toEqual(
+			"http://127.0.0.1:4003/api/transactions/fees",
+		);
 		expect(arkApiService.validateAddress(VALID_ADDRESS)).toEqual(true);
 	});
 
-	it("should get a delegate by public key", (done) => {
+	it("should get a delegate by public key", async () => {
+		const spy = spyOn(HttpClient.prototype, "get" as any).and.callThrough();
+
 		// Activate network to have a client
 		const userDataService = arkApiSpectator.get(UserDataService);
 		userDataService.onActivateNetwork$.next(currentNetwork);
 
-		arkApiSpectator.expectConcurrent([
-			{
-				url: "http://127.0.0.1:4003/api/peers",
-				method: HttpMethod.GET,
-			},
-			{
-				url: "http://127.0.0.1:4003/api/transactions/fees",
-				method: HttpMethod.GET,
-			},
-		]);
-
-		arkApiService.getDelegateByPublicKey(PUBLIC_KEY).subscribe({
-			next: (data) => {
-				expect(data.username).toEqual(delegatesFixture[0].username);
-				done();
-			},
-		});
-
-		const req = arkApiSpectator.expectOne(
-			"http://127.0.0.1:4003/api/delegates/02d29ba7ebe8823137f31ed8e71288274a1cefa23a98a94fa62d4124817d58b777",
-			HttpMethod.GET,
+		expect(spy.calls.argsFor(0)[0]).toEqual(
+			"http://127.0.0.1:4003/api/peers",
+		);
+		expect(spy.calls.argsFor(1)[0]).toEqual(
+			"http://127.0.0.1:4003/api/transactions/fees",
 		);
 
-		req.flush({ data: delegatesFixture[0] });
+		const data = await arkApiService
+			.getDelegateByPublicKey(PUBLIC_KEY)
+			.toPromise();
+
+		expect(data.username).toEqual(delegatesFixture[0].username);
+		expect(spy.calls.argsFor(2)[0]).toEqual(
+			"http://127.0.0.1:4003/api/delegates/02d29ba7ebe8823137f31ed8e71288274a1cefa23a98a94fa62d4124817d58b777",
+		);
 	});
 });
